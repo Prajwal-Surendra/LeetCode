ac15210798c059e1bc95b0e8acdcf6551fb19bc8
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2022-10-20 20:49:07 +0530

    Time: 1 ms, Memory: 39.7 MB - LeetHub

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -4,9 +4,7 @@ class Solution {
         while(x != 0)
         {
             result = (result*10) + (x%10);
-            if(result > Integer.MAX_VALUE) 
-                return 0;
-            if(result < Integer.MIN_VALUE) 
+            if(result > Integer.MAX_VALUE || result<Integer.MIN_VALUE) 
                 return 0;
             x = x/10;
         }
