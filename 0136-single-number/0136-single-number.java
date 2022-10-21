57bc7197ff7423ca035e5f69e9068e33542157cd
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2022-10-21 21:32:54 +0530

    Time: 4 ms (60.18%), Space: 50 MB (76.83%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int singleNumber(int[] nums) 
-    {
+    public int singleNumber(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++)
             sum^=nums[i];
