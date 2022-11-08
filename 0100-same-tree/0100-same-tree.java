b2b4b22b109506024e605709ccb4be2fec98a7ef
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2022-11-08 06:52:45 +0530

    Time: 0 ms (100.00%), Space: 41 MB (76.25%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -19,8 +19,6 @@ class Solution {
             return true;
         if(p==null || q==null)
             return false;
-        if(p.val != q.val)
-            return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
