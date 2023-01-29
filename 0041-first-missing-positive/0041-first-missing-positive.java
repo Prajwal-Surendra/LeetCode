a26d77b45423e57ab00201d185e8a1ed4b87072c
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2023-01-29 12:37:30 +0530

    Time: 1 ms (100.00%), Space: 50.5 MB (85.01%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    static void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
