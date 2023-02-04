68ad0139f10494deda2d08f46a5c7267eb9a8897
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2023-02-04 11:41:38 +0530

    Time: 5 ms (43.89%), Space: 41.8 MB (80.68%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,13 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int x1 = 0, x2 = 0, mask = 0;
-        for (int i : nums) {
-            x2 ^= x1 & i;
-            x1 ^= i;
-            mask = ~(x1 & x2);
-            x2 &= mask;
-            x1 &= mask;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            if (sum != 0) {
+                result |= sum << i;
+            }
         }
-        return x1;
+        return result;
     }
 }
