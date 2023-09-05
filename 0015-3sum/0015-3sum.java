f0bc4eac8cff204274babf17b42b1656c4d4f14b
Prajwal-Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2023-09-05 18:41:18 +0530

    Time: 31 ms (79.45%), Space: 51.7 MB (19.38%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
+        LinkedList<List<Integer>> ans = new LinkedList<List<Integer>>();
 
         for (int i = 0; i < nums.length - 2; i++) {
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
@@ -11,11 +11,11 @@ class Solution {
 
                 while (left < right) {
                     if (nums[left] + nums[right] == target) {
-                        ArrayList<Integer> miniSol = new ArrayList<>();
-                        miniSol.add(nums[i]);
-                        miniSol.add(nums[left]);
-                        miniSol.add(nums[right]);
-                        sol.add(miniSol);
+                        ArrayList<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        ans.add(list);
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
@@ -33,6 +33,7 @@ class Solution {
             }
         }
 
-        return sol;
+        return ans;
+        
     }
 }
