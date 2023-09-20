b5e9d137bba272b5710a94681c44f66579a65655
Prajwal Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2023-09-21 01:30:45 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (7.04%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,84 +1,48 @@
 class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> allboards = new ArrayList<>();
+     public static List < List < String >> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        helper(allboards, board, 0);
-        return allboards;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List < List < String >> res = new ArrayList < List < String >> ();
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+        return res;
     }
-    public void helper(List<List<String>> allboards, char[][] board, int cols) {
-        if(cols==board.length) {
-            saveBoard(allboards, board);
+
+
+
+    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(construct(board));
             return;
         }
-        
-        for(int row = 0 ; row <board.length; row++) {
-            if(isSafe(row,cols,board)) {
-                board[row][cols] = 'Q';
-                helper(allboards, board, cols+1);
-                board[row][cols] = '.';
+
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
             }
         }
     }
-    
-    public boolean isSafe(int row, int cols, char[][] board) {
-        for(int i = 0; i<board.length; i++) {
-            if(board[i][cols] == 'Q') {
-                return false;
-            }
+
+
+    static List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
         }
-        
-        for(int i = 0; i<board.length;i++) {
-            if(board[row][i] == 'Q') {
-                return false;
-            }
-        }
-        
-        int r=row;
-        for(int c=cols; c>=0 && r>=0; c--,r--) {
-            if(board[r][c] == 'Q') {
-                return false;
-            }
-        }
-        
-        r=row;
-        for(int c=cols; c<board.length && r>=0; c++,r--) {
-            if(board[r][c] == 'Q') {
-                return false;
-            }
-        }
-        
-        r=row;
-        for(int c=cols; c>=0 && r<board.length; c--,r++) {
-            if(board[r][c] == 'Q') {
-                return false;
-            }
-        }
-        
-        r=row;
-        for(int c=cols; c<board.length && r<board.length; c++,r++) {
-            if(board[r][c] == 'Q') {
-                return false;
-            }
-        }
-        
-        return true;
+        return res;
     }
-    
-    public void saveBoard(List<List<String>> allboard, char[][] board) {
-        String row="";
-        List<String> list = new ArrayList<>();
-        for(int i =0 ; i<board.length;i++) {
-            row="";
-            for(int j=0;j<board[i].length;j++) {
-                if(board[i][j] == 'Q') {
-                    row+='Q';
-                }
-                else {
-                    row+='.';
-                }
-            }
-            list.add(row);
-        }
-        allboard.add(list);
-    }
+
 }
