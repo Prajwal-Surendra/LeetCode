8cd8a627e907c898a00ccccdeada9f8facc86b8b
Prajwal Surendra <85682152+Prajwal-Surendra@users.noreply.github.com>
2023-09-30 17:39:38 +0530

    Time: 2 ms (98.47%), Space: 46.8 MB (30.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        int count1=0, count2=0, el1=Integer.MIN_VALUE, el2=Integer.MIN_VALUE;
+        
+        for(int i =0; i<nums.length;i++) {
+            if(count1==0 && nums[i]!=el2) {
+                count1++;
+                el1=nums[i];
+            }    
+            else if(count2==0 && nums[i]!=el1) {
+                count2++;
+                el2=nums[i];
+            }
+            else if(nums[i] == el1) {
+                count1++;
+            }
+            else if(nums[i] == el2) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
         for(int i:nums) {
-            map.put(i,map.getOrDefault(i,0)+1);
+            if(i==el1) {
+                count1++;
+            }
+            if(i==el2) {
+                count2++;
+            }
         }
         
-        List<Integer> list = new ArrayList<>();
-        for(int i: nums) {
-            int feq = map.getOrDefault(i,0);
-            if(feq>(nums.length/3)) {
-                if(!list.contains(i)) {
-                    list.add(i);    
-                }
-            }
+        int min = (int)(nums.length/3) ;
+        if(count1>min) {
+            list.add(el1);
+        }
+        if(count2>min) {
+            list.add(el2);
         }
         return list;
     }
